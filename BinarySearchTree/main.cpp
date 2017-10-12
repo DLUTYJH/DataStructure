f6c34de84a1fd36919fc3f4aea544610cafc3f04
DLUTYJH <1161051479@qq.com>
2017-10-12 19:12:34 +0000

    添加合并删除操作

--- BinarySearchTree/main.cpp
+++ BinarySearchTree/main.cpp
@@ -3,6 +3,8 @@
 #include "binarysearch.h"
 #include "BST.h"
 #include <stack>
+#include "SequenceST.h"
+#include <cstring>
 
 using namespace std ;
 
@@ -45,35 +47,76 @@ int main() {
     cout<<"**************"<<endl ;
     //bst.preOrderByrecusion();
     //bst.preOrderWithoutRecusion();
-    cout<<"**************"<<endl ;
+   // cout<<"**************"<<endl ;
     //bst.inOrerByRecusion();
    // bst.inOrderWithoutRecusion();
-    cout<<"**************"<<endl ;
-    //bst.postOrderRecusion();
+    //cout<<"**************"<<endl ;
+    ///bst.postOrderRecusion();
     //bst.postOrderWithoutRecusion();
 
-    cout<< " 是否找到 key  : "<<bst.containWithoutRecusion(0)<<endl ;
-    cout<< " 是否找到 key  : "<<bst.containByRecusion(0)<<endl ;
+   // cout<< " 是否找到 key  : "<<bst.containWithoutRecusion(0)<<endl ;
+    //cout<< " 是否找到 key  : "<<bst.containByRecusion(0)<<endl ;
 
-    cout<< " value =  : "<<*bst.searchWithoutRecusion(0)<<endl ;
-    cout<< " value =  : "<<*bst.searchByRecsion(0)<<endl ;
+    //cout<< " value =  : "<<*bst.searchWithoutRecusion(0)<<endl ;
+    //cout<< " value =  : "<<*bst.searchByRecsion(0)<<endl ;
 
-    cout<<"min = "<<*bst.min()<<endl ;
-    cout<<"max = "<<*bst.max()<<endl ;
+    //cout<<"min = "<<*bst.min()<<endl ;
+    //out<<"max = "<<*bst.max()<<endl ;
 
     //bst.removeMin();
     //bst.removeMinByRecusion();
    // bst.levelOrder();
 
-    cout<<"**************"<<endl ;
+    //cout<<"**************"<<endl ;
     //bst.removeMax();
     //bst.removeMaxByRecusion();
    // bst.levelOrder();
 
     //bst.remove(2);
-    bst.removeByMerge(9);
-    bst.levelOrder();
-    cout<<"dfaaa"<<endl ;
+    //bst.removeByMerge(9);
+    //bst.levelOrder();
+   // cout<<"dfaaa"<<endl ;
+
+    SequenceST<string ,int> sst ;
+
+    sst.insertNode("hello ",1);
+    sst.insertNode("hello ",11);
+    sst.insertNode("hello11",2);
+    sst.insertNode("hello11",22);
+    sst.insertNode("hello22",3);
+    sst.insertNode("hello22",33);
+    sst.insertNode("hello33",4);
+    sst.insertNode("hello33",44);
+    sst.insertNode("hello44",5);
+    sst.insertNode("hello44",55);
+    sst.insertNode("hello55",6);
+    sst.insertNode("hello55",66);
+    sst.insertNode("hello66",7);
+    sst.insertNode("hello66",77);
+    sst.insertNode("hello77",8);
+    sst.insertNode("hello77",88);
+    sst.insertNode("hello88",9);
+    sst.insertNode("hello88",99);
+    sst.insertNode("hello99",10);
+    sst.insertNode("hello99",1010);
+
+    sst.print();
+
+    cout<<"**************"<<endl ;
+    cout<< "是否存在 key = hello11 的值 ： "<<sst.contain("hello11")<<endl ;
+
+    cout<<"**************"<<endl ;
+    cout<< "是否存在 key = hello11 ,value =  ： "<<*sst.search("hello11")<<endl ;
+
+    cout<<"**************"<<endl ;
+    sst.remove("hello11");
+    sst.print();
+
+
+
+
+
+
 
 
 
